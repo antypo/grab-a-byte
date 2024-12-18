999a31b0e472935991402d2941594dc4124e72c6
antypo <hubert.wyskwar@gmail.com>
2024-12-19 00:11:04 +0100

    GRAB-1 Make tests OS agnostic

--- src/test/java/com/antypo/grababyte/order/CustomerServiceTest.java
+++ src/test/java/com/antypo/grababyte/order/CustomerServiceTest.java
@@ -16,6 +16,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * Tests for customer service. It is lacking some coverage, but I wanted to show the general idea.
+ * It's possible to mock going through all of the methods.
+ */
 class CustomerServiceTest {
 
     @Mock
@@ -36,18 +40,19 @@ class CustomerServiceTest {
         Scanner mockScanner = mock(Scanner.class);
         when(mockScanner.nextInt()).thenReturn(2);
         CustomerService.setScanner(mockScanner);
-        String expectedOutput = """
-                Hello and welcome to Grab a Byte! We offer up to petabytes of delicious lunches and streams of nourishing drinks.\s
-                Would you like to make an order?
-                 1. Yes    or    2. No
-                Please select an option:\s
-                
-                Thank you for using our services! Hoping to work together in the future!
-                
-                Oh they're gone! Here is today's summary:
-                Today's revenue is *DRUMROLL*: 0,00zł.
-                Top selling items were:
-                """;
+
+        String expectedOutput = "Hello and welcome to Grab a Byte! We offer up to petabytes of delicious lunches and " +
+                "streams of nourishing drinks. " + System.lineSeparator() +
+                "Would you like to make an order?" + System.lineSeparator() +
+                " 1. Yes    or    2. No" + System.lineSeparator() +
+                "Please select an option: " + System.lineSeparator() +
+                System.lineSeparator() +
+                "Thank you for using our services! Hoping to work together in the future!" + System.lineSeparator() +
+                System.lineSeparator() +
+                "Oh they're gone! Here is today's summary:" + System.lineSeparator() +
+                "Today's revenue is *DRUMROLL*: 0,00zł." + System.lineSeparator() +
+                "Top selling items were:" + System.lineSeparator();
+
 
         // Capture the output
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -67,15 +72,16 @@ class CustomerServiceTest {
 
         // Important parts to be contained in output
         String expectedIntro = "Hello and welcome to Grab a Byte! We offer up to petabytes of delicious lunches and streams of nourishing drinks.";
-        String expectedIngredients = "The main course you've selected contains the following ingredients:\nHubert";
+        String expectedIngredients = "The main course you've selected contains the following ingredients:" + System.lineSeparator() + "Hubert";
         String expectedAddedToOrderInfo = "Christmas Egg with Sernik has been added to your order! Your current total is: 13011,99zł.";
-        String expectedOrderSummary = "1. 1x Christmas Egg with Sernik for 13011,99zł\n\nYour current total is: 13011,99zł.";
-        String expectedDaySummary = """
-                Oh they're gone! Here is today's summary:
-                Today's revenue is *DRUMROLL*: 13011,99zł.
-                Top selling items were:
-                Christmas Egg - ordered 1 times
-                Sernik - ordered 1 times""";
+        String expectedOrderSummary = "1. 1x Christmas Egg with Sernik for 13011,99zł" + System.lineSeparator() + System.lineSeparator() +
+                "Your current total is: 13011,99zł.";
+        String expectedDaySummary = "Oh they're gone! Here is today's summary:" + System.lineSeparator() +
+                "Today's revenue is *DRUMROLL*: 13011,99zł." + System.lineSeparator() +
+                "Top selling items were:" + System.lineSeparator() +
+                "Christmas Egg - ordered 1 times" + System.lineSeparator() +
+                "Sernik - ordered 1 times";
+
 
         // Mock the responses to make an order
         when(mockScanner.nextInt()).thenReturn(1)
